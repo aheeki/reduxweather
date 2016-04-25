a9b6ab62827ba72bda01baa4358260ddb7209552
Andrew Heekin <andrewheekin@gmail.com>
2016-04-25 15:09:17 -0400

    simplified webpack config

--- webpack.config.docker.js
+++
@@ -1,7 +0,0 @@
-var config = require('./webpack.config.base.js')('development');
-
-config.watchOptions = {
-  poll: "6500"
-};
-
-module.exports = config;
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,38 @@
-module.exports = require('./webpack.config.base.js')('production');
+var webpack = require('webpack');
+var path = require('path');
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+
+module.exports = {
+  entry: {
+    entry: './src/index.js'
+  },
+  output: {
+    path: '/bundle/',
+    publicPath: '/bundle/',
+    filename: '[name].js'    
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: [ path.resolve(__dirname, "src") ],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          plugins: ['transform-runtime'],
+          presets: ['es2015', 'stage-0', 'react']
+        }
+      },
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+    ]
+  },
+  plugins: [
+    commonsPlugin,
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': "development" })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.json', '.less']
+  }
+};
