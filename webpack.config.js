3034745f1d6a72c261e12f9b60b6fbb28b91d2c8
Andrew Heekin <andrewheekin@gmail.com>
2016-04-30 11:02:16 -0400

    static refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,50 @@
+var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var webpack = require('webpack')
+
+var locals = {
+  paths: [
+    '/'
+  ]
+}
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: {
+    main: './src/index.js',
+    css: './scss/app.scss'
+  },
   output: {
     path: __dirname,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    libraryTarget: 'umd'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel'
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: { presets: ['es2015', 'react'] }
+      },
+      {
+        test: /\.scss/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          '!css-loader!sass-loader'
+        )
+      }
+    ]
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './'
-  }
+  },
+  plugins: [
+    new StaticSiteGeneratorPlugin('main', locals.paths, locals),
+    new ExtractTextPlugin('style.css'),
+    new webpack.NoErrorsPlugin()    
+  ],
+  watch: true
 };
